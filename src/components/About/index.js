d112a719ce7bbe9b2ef2ad644e55f924f8c25b1a
akarsh-mahajan <akarshmahajan23@gmail.com>
2024-01-30 16:16:49 +0530

    changes in symbols in cube at about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,8 +3,9 @@ import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
+import {faCss3, faGitAlt, faHtml5, faPython, faJava} from '@fortawesome/free-brands-svg-icons'
 import Loader from 'react-loaders'
+import {faDatabase} from '@fortawesome/free-solid-svg-icons';
 
 function About() {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -41,7 +42,7 @@ function About() {
         <div className="stage-cube-cont">
             <div className="cubespinner">
                 <div className="face1">
-                    <FontAwesomeIcon icon={faAngular} color="#DD0031"/>
+                    <FontAwesomeIcon icon={faDatabase} color="#DD0031"/>
                 </div>
                 <div className="face2">
                     <FontAwesomeIcon icon={faHtml5} color="#F06529"/>
@@ -50,10 +51,10 @@ function About() {
                     <FontAwesomeIcon icon={faCss3} color="#28A4D9"/>
                 </div>
                 <div className="face4">
-                    <FontAwesomeIcon icon={faReact} color="#5ED4F4"/>
+                    <FontAwesomeIcon icon={faPython} color="#5ED4F4"/>
                 </div>
                 <div className="face5">
-                    <FontAwesomeIcon icon={faJsSquare} color="#EFD81D"/>
+                    <FontAwesomeIcon icon={faJava} color="#EFD81D"/>
                 </div>
                 <div className="face6">
                     <FontAwesomeIcon icon={faGitAlt} color="#EC4D28"/>
